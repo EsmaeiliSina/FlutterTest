777e91c1618c54666c8b670ab85da4df8f1e482c
EsmaeiliSina <es.sina76@gmail.com>
2021-01-26 14:28:27 +0330

    extract sidebar row to class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app/components/sidebaritem_row.dart';
+import 'package:app/model/sidebar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,35 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: Center(
-        child: Row(
-          children: [
-            Container(
-                width: 42,
-                height: 42,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14.0),
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                      colors: [Colors.yellow, Colors.orange]),
-                ),
-                child: Icon(
-                  Icons.home,
-                  color: Colors.white,
-                )),
-            SizedBox(
-              width: 12,
-            ),
-            Container(
-              child: Text(
-                'Home',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
-              ),
-            ),
-          ],
+        child: SidebarRow(
+          item: sidebarItems[0],
         ),
       )),
     );
