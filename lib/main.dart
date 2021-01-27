1f7ab850d8ad084ad7656af98a8cf44358df7b99
EsmaeiliSina <es.sina76@gmail.com>
2021-01-27 14:51:27 +0330

    complete navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app/constants.dart';
 import 'package:app/model/course.dart';
+import 'package:app/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'components/cards/recent_course_cards.dart';
@@ -11,13 +13,118 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: RecentCourseCard(
-              course: recentCourses[1],
+        body: Container(
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              children: [
+                HomeScreenNavBar(),
+                RecentCourseCard(
+                  course: recentCourses[0],
+                )
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SideBarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(width: 16),
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(left: 12, right: 16),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              boxShadow: [
+                BoxShadow(
+                    color: kShadowColor, offset: Offset(0, 12), blurRadius: 16),
+              ]),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              cursorColor: kPrimaryLabelColor,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20,
+                ),
+                border: InputBorder.none,
+                hintText: 'Serach for Courses',
+                hintStyle: kSearchPlaceholderStyle,
+              ),
+              style: kSearchTextStyle,
+              onChanged: (newText) {
+                print(newText);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SideBarButton extends StatelessWidget {
+  const SideBarButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print('sina');
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(maxHeight: 40, maxWidth: 40),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [
+            BoxShadow(
+                color: kShadowColor, offset: Offset(0, 12), blurRadius: 16),
+          ],
+        ),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
       ),
     );
   }
