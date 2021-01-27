7c3aef4f7b8a533537b2e5cdebd9a900edc264e1
EsmaeiliSina <es.sina76@gmail.com>
2021-01-27 11:49:14 +0330

    add cards file

---
+++ lib/components/cards/recent_course_cards.dart
@@ -0,0 +1,90 @@
+import 'package:app/model/course.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+// ignore: must_be_immutable
+class RecentCourseCard extends StatelessWidget {
+  RecentCourseCard({this.course});
+  Course course;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 25.0),
+          child: Container(
+            height: 240,
+            width: 240,
+            decoration: BoxDecoration(
+              gradient: course.background,
+              borderRadius: BorderRadius.circular(41),
+              boxShadow: [
+                BoxShadow(
+                    color: course.background.colors[0].withOpacity(0.3),
+                    offset: Offset(0, 20),
+                    blurRadius: 30),
+                BoxShadow(
+                    color: course.background.colors[1].withOpacity(0.3),
+                    offset: Offset(0, 20),
+                    blurRadius: 30),
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 32,
+                    right: 32,
+                    left: 32,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        course.courseSubtitle,
+                        style: kCardSubtitleStyle,
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        course.courseTitle,
+                        style: kCardTitleStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'asset/illustrations/${course.illustration}',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 42),
+          child: Container(
+            child: Image.asset(
+              'asset/logos/${course.logo}',
+              scale: 2.8,
+            ),
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: [
+                BoxShadow(
+                    color: kShadowColor, offset: Offset(0, 4), blurRadius: 16),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
