eaf4be6348f55b9b34869d035839828b7274c76a
EsmaeiliSina <es.sina76@gmail.com>
2021-02-12 10:51:06 +0330

    work with image widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
-
-import 'components/home_screen_navbar.dart';
-import 'components/lists/recent_course_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,39 +6,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          color: kBackgroundColor,
-          child: SafeArea(
-            child: Column(
-              children: [
-                HomeScreenNavBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'Recents',
-                        style: kLargeTitleStyle,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        '23 courses, more is coming',
-                        style: kSubtitleStyle,
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RecentCourseList(),
-              ],
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://www.itl.cat/pngfile/big/12-122105_diamond-background-wallpapers-diamond-with-black-background.jpg"),
           ),
         ),
       ),
